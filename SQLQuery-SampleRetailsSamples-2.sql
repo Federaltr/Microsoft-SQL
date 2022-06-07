563ca285490cd77356131c195e342452624fe3bb
Halil Aydın <hllaydnyzn@gmail.com>
2022-06-07 23:16:48 +0300

    some updates

--- SQLQuery-SampleRetailsSamples-2.sql
+++ SQLQuery-SampleRetailsSamples-2.sql
@@ -2,16 +2,18 @@
 
 ---- 1. List all the cities in the Texas and the numbers of customers in each city.----
 
-  SELECT customer.customer_id, customer.city
+  SELECT customer.city, COUNT(customer.customer_id) AS number_of_customer
   FROM sale.customer
-  WHERE state = 'TX';
+  WHERE state = 'TX'
+  GROUP BY city;
 
----- 2. List all the cities in the California which has more than 5 customer, by showing the cities which have more customers first.---
+---- 2. List all the cities in the California which has more than 35 customer, by showing the cities which have more customers first.---
 
-  SELECT customer.city, COUNT(customer_id) as customer_count
+  SELECT customer.city, COUNT(customer_id) AS customer_count
   FROM sale.customer
   WHERE state = 'CA'
   GROUP BY city
+  HAVING COUNT(customer_id) > 35  --where'de aggregate func. kullanýlamadýðý için having kullandýk.
   ORDER BY COUNT(customer_id) DESC
 
 ---- 3. List the top 10 most expensive products----
@@ -24,7 +26,7 @@
 
   SELECT B.store_id, A.product_name, A.list_price, B.quantity
   FROM product.product A
-  LEFT JOIN product.stock B
+  INNER JOIN product.stock B
   ON A.product_id = B.product_id
   WHERE (store_id = 2) AND (quantity > 25)
   ORDER BY quantity DESC;
@@ -33,7 +35,7 @@
 
   SELECT A.customer_id, A.order_date, B.city
   FROM sale.orders A
-  LEFT JOIN sale.customer B
+  INNER JOIN sale.customer B
   ON A.customer_id = B.customer_id
   WHERE city = 'Boulder'
   ORDER BY order_date;
@@ -49,18 +51,43 @@
 
 ---- 7. What is the sales quantity of product according to the brands and sort them highest-lowest----
 
-
+  SELECT b.brand_name, COUNT(c.quantity) as brand_quantity
+  FROM product.product A
+  LEFT JOIN product.brand B
+  ON A.brand_id = B.brand_id
+  LEFT JOIN sale.order_item C
+  ON A.product_id = c.product_id
+  GROUP BY B.brand_name
+  ORDER BY COUNT(c.quantity) DESC
 
 ---- 8. What are the categories that each brand has?----
 
+  SELECT C.brand_name, B.category_name
+  FROM product.product A
+  LEFT JOIN product.category B
+  ON A.category_id = B.category_id
+  LEFT JOIN product.brand C
+  ON A.brand_id = C.brand_id
+  GROUP BY brand_name, category_name
+  ORDER BY brand_name
 
 ---- 9. Select the avg prices according to brands and categories----
 
+  	 
 
 ---- 10. Select the annual amount of product produced according to brands----
 
+  SELECT A.model_year, B.brand_name, SUM(c.quantity) AS year_quantity
+  FROM product.product A
+  LEFT JOIN product.brand B
+  ON A.brand_id = B. brand_id
+  LEFT JOIN product.stock C
+  ON A.product_id = C.product_id
+  GROUP BY model_year, brand_name
+  ORDER BY model_year DESC 
 
 ---- 11. Select the store which has the most sales quantity in 2016.----
+  
 
 
 ---- 12 Select the store which has the most sales amount in 2018.----
