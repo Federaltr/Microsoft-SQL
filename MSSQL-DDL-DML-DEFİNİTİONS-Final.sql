0a672fc80f3aca102ea934c25cf3a98c582c8488
Halil Aydın <hllaydnyzn@gmail.com>
2022-06-11 14:13:20 +0300

    some updates

--- MSSQL-DDL-DML-DEFİNİTİONS-Final.sql
+++ MSSQL-DDL-DML-DEFİNİTİONS-Final.sql
@@ -1,33 +1,35 @@
+--Key Terms
+--DDL: abbreviation for data definition language
+--DML: abbreviation for data manipulation language
+--SEQUEL: acronym for Structured English Query Language; designed to manipulate and retrieve data stored in IBM’s quasi-relational database management system, System R
+--Structured Query Language(SQL): a database language designed for managing data held in a relational database management system
+
 --Data Definition Language (DDL)
 --Perform basic data management chores (add, delete and modify)
 
 --SHOW DATABASES
-SELECT name FROM sys.databases
-
-SELECT name FROM master.dbo.sysdatabases 
-WHERE name NOT IN ('master', 'tempdb', 'model', 'msdb');
-
 EXEC sp_databases
 
+--SHOW DATABASES
+SELECT name FROM sys.databases
+
+--SHOW DATABASES not in SYSTEM
+SELECT name FROM master.dbo.sysdatabases 
+WHERE name NOT IN ('master', 'tempdb', 'model', 'msdb');
+GO
 
 --CREATE DATABASE
 --The major SQL DDL statements are. CREATE DATABASE
 CREATE DATABASE new_db;
+GO
 
 USE new_db;
-
-EXECUTE sp_helpindex departments;
-
---RENAME COLUMN
-EXEC sp_rename 'Tmp_Names', 'Names'
---rename new column to old column name
-EXEC sp_rename 'table1.col1', 'oldcol1', 'column'
-
+GO
 
 --CREATE TABLE
 --Table name is the name of the database table such as departments. Each field in the CREATE TABLE has three parts: ColumnName, Data type, Optional Column Constraint, Table Constraint
 CREATE TABLE departments(
-	id BIGINT NOT NULL,
+	id BIGINT NOT NULL IDENTITY(1,1),
 	name VARCHAR(20) NULL,
 	dept_name VARCHAR(20) NULL,
 	seniority VARCHAR(20) NULL,
@@ -35,9 +37,51 @@ CREATE TABLE departments(
 	salary BIGINT NULL,
 	hire_date DATE NULL,
     
-	CONSTRAINT pk_1 
-	PRIMARY KEY (id)
+	CONSTRAINT pk_1 PRIMARY KEY (id)
 );
+GO
+
+--SEE KEYs in TABLE
+EXECUTE sp_helpindex departments;
+GO
+
+--RENAME
+--select * from departments;
+USE new_db;
+
+/*-SYNTAX
+EXEC sp_rename 
+	@objname = N'index_name', 
+	@newname = N'new_index_name',   
+	@objtype = N'INDEX';
+
+-- Return the current Primary Key, Foreign Key and Check constraints for the departments table.  
+SELECT	name, type_desc
+		,SCHEMA_NAME(schema_id) AS schema_name		
+FROM	sys.objects  
+WHERE	parent_object_id = (OBJECT_ID('departments'))   
+		AND type IN ('C','F', 'PK'); 
+*/
+
+--RENAME Table Name
+EXEC sp_rename 'departments', 'departments2';
+EXEC sp_rename 'departments2', 'departments';
+
+--RENAME Column
+EXEC sp_rename 'departments.dept_name', 'dept_name2', 'COLUMN';  
+EXEC sp_rename 'departments.dept_name2', 'dept_name', 'COLUMN';  
+
+--RENAME Index
+USE new_db;
+EXEC sp_rename 'departments.pk_1', 'pk_id', 'INDEX'
+EXEC sp_rename 'departments.pk_id', 'pk_1', 'INDEX' 
+
+-- Rename the primary key constraint.
+-- Rename a check constraint.
+-- Rename a foreign key constraint.  
+EXEC sp_rename 'departments.pk_1', 'pk_id'
+EXEC sp_rename 'departments.pk_id', 'pk_1'
+GO
 
 --ALTER TABLE
 --ALTER TABLE allows columns to be removed.
@@ -59,46 +103,59 @@ UNIQUE (id);
 ALTER TABLE departments
 DROP CONSTRAINT unique_id_constraint;
 
+--ALTER ADD COLUMN
+ALTER TABLE departments
+ADD seniority2 VARCHAR(20) NULL;
+
 --ALTER COLUMN CHANGE DATA STRUCTURE
 ALTER TABLE departments
-ALTER COLUMN dept_name VARCHAR(50) NOT NULL;
+ALTER COLUMN seniority2 VARCHAR(50) NOT NULL;
 
 --ALTER DROP COLUMN
 ALTER TABLE departments
-DROP COLUMN seniority;
+DROP COLUMN seniority2;
 
---ALTER ADD COLUMN
-ALTER TABLE departments
-ADD seniority VARCHAR(20) NULL;
-
+/*
 --DROP TABLE
 --The DROP TABLE will remove a table from the database. Make sure you have the correct database selected.
 DROP TABLE departments;
-
+--OR
 DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS #create_salary;
+DROP TABLE IF EXISTS #into_salary;
+DROP VIEW IF EXISTS view_salary;
 
 --DROP DATABASE
 --The DROP DATABASE command is used to delete an existing SQL database.
-DROP DATABASE new_db;
+USE [master];
+EXEC msdb.dbo.sp_delete_database_backuphistory @database_name = N'new_db'
+ALTER DATABASE [new_db] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+DROP DATABASE IF EXISTS [new_db]
+*/
+GO
 
---Key Terms
---DDL: abbreviation for data definition language
---DML: abbreviation for data manipulation language
---SEQUEL: acronym for Structured English Query Language; designed to manipulate and retrieve data stored in IBM’s quasi-relational database management system, System R
---Structured Query Language(SQL): a database language designed for managing data held in a relational database management system
 
 --Data Manipulation Language (DML)
 --SELECT – to query data in the database
 --INSERT – to insert data into a table
 --UPDATE – to update data in a table
 --DELETE – to delete data from a table
+GO
 
-SELECT * 
-FROM departments;
+--SELECT DISTINCT item(s)
+--FROM table(s)
+--WHERE predicate
+--GROUP BY field(s)
+--ORDER BY fields;
+
+--SELECT * FROM	departments;
+GO
 
 --INSERT
 --INSERT [INTO] Table_name | view name [column_list]
---DEFAULT VALUES | values_list | select statement
+--DEFAULT VALUES | values_list | select statementSET 
+--USE IDENTITY_INSERT because of id-IDENTITY
+SET IDENTITY_INSERT departments ON;
 INSERT departments (id, name, dept_name, seniority, graduation, salary, hire_date)
 VALUES
 (10238,	'Eric'	   ,'Economics'	       ,'Experienced'	,'MSc' ,72000	,'2019-12-01'),
@@ -114,16 +171,8 @@ VALUES
 (64378,	'Elvis'	   ,'Physics'	       ,'Senior'	,'MSc' ,87000	,'2018-11-23'),
 (96945,	'John'	   ,'Computer Science' ,'Experienced'	,'MSc' ,80000	,'2019-04-20'),
 (99231,	'Santosh'  ,'Computer Science' ,'Experienced'	,'BSc' ,74000	,'2020-05-07');
-
---SELECT DISTINCT item(s)
---FROM table(s)
---WHERE predicate
---GROUP BY field(s)
---ORDER BY fields;
-
-SELECT TOP 2 id, name, dept_name
-FROM departments
-ORDER BY id;
+SET IDENTITY_INSERT departments OFF;
+GO
 
 --To allow an insert with a specific identity value, the IDENTITY_INSERT option should be used.
 --You can't alter the existing columns for identity.
@@ -131,51 +180,56 @@ ORDER BY id;
 --Create a new table with identity & drop the existing table
 --Create a new column with identity & drop the existing column
 SET IDENTITY_INSERT departments ON;
-
 INSERT departments (id, name, dept_name, seniority, graduation, salary, hire_date)
 VALUES (44552,	'Edmond' ,'Economics'	,'Candidate','BSc' ,60000	,'2021-12-04')
-
 SET IDENTITY_INSERT departments OFF;
+GO
 
 --Insert with SELECT
 --You should use # for creating a temporary table.
-CREATE TABLE #salary (
-id BIGINT NOT NULL,
-name VARCHAR (40) NULL,
-salary BIGINT NULL
+CREATE TABLE #create_salary (
+id		BIGINT NOT NULL,
+name	VARCHAR (40) NULL,
+salary	BIGINT NULL
 );
 
-SELECT * 
-FROM #salary;
-
 --ADDING SAMPLE QUERY VALUES
-INSERT #salary
-SELECT id, name, salary 
-FROM departments;
+INSERT	#create_salary
+SELECT	id, name, salary 
+FROM	departments;
+GO
 
 --Or you can use the SELECT ... INTO ... FROM statement as follow:
 --CREATE TABLE AND ADDING
-SELECT id, name, salary 
-INTO #salary
-FROM departments;
+SELECT	id, name, salary 
+INTO	#into_salary
+FROM	departments;
+GO
+
+--Or you can use the VIEW
+--CREATE OR ALTER VIEW view_salary as 
+SELECT	id, name, salary
+FROM	departments;
+GO
 
 --CREATE COPY TABLE
 SELECT *
 INTO departments_copy
 FROM departments;
-
-SELECT * 
-FROM departments_copy;
+GO
 
 --UPDATE
 UPDATE departments
 SET name = 'Edward'
 WHERE id = 10238;
 
-SELECT * 
-FROM departments
-WHERE id = 10238;
+--SHOW TABLES
+SELECT * FROM #create_salary;
+SELECT * FROM #into_salary;
+SELECT * FROM departments_copy;
+GO
 
+/*
 --DELETE
 --DELETE [FROM] {table_name | view_name }
 --[WHERE clause];
@@ -185,3 +239,31 @@ DELETE FROM departments;
 
 --Deleting selected rows:
 DELETE FROM departments WHERE id = 10238;
+*/
+GO
+
+/*
+--OPTIONAL
+--WITH common_table_expression (Transact-SQL)
+
+--WITH temporaryTable (averageValue) 
+--as (
+--SELECT avg(Attr1) FROM Table
+--)
+--SELECT Attr1
+--FROM Table, temporaryTable
+--WHERE Table.Attr1 > temporaryTable.averageValue;
+
+--RUN ONLY ONE GREEN AND RUN UNDER CERTAIN CONDITION
+DROP VIEW IF EXISTS view_salary;
+DROP TABLE IF EXISTS #into_salary;
+
+--CREATE OR ALTER VIEW view_salary as 
+WITH deprt_temp (customer_id, first_name, last_name, product_name) 
+AS (
+	SELECT * FROM departments
+)
+SELECT * INTO #into_salary FROM main_table_temp
+--SELECT * FROM deprt_temp
+GO
+*/
---
+++ SQL-Session6(Subqueries-CTE's).sql
@@ -0,0 +1,228 @@
+
+ 
+--- SUBQUERIES(NESTED QUERIES) ---
+
+-- In SELECT Clause
+
+SELECT order_id,
+	   list_price,
+	   (
+	   SELECT AVG(list_price)  -- tüm satýrlar için ayný deðer döndü.
+	   FROM product.product
+	   ) AS avg_price
+FROM sale.order_item
+;
+
+-- In WHERE Clause
+
+SELECT order_id, order_date
+FROM sale.orders
+WHERE order_date IN (
+	                 SELECT TOP 5 order_date
+					 FROM sale.orders
+					 ORDER BY order_date DESC
+                    )
+;
+
+SELECT TOP 5 order_id, order_date  --yukardaki kodla ayný sonuç
+FROM sale.orders
+ORDER BY order_date  DESC
+
+-- In FROM Clause
+
+SELECT order_id, order_date
+FROM	(
+		 SELECT TOP 5 *
+		 FROM sale.orders
+		 ORDER BY order_date DESC
+		) A  --Alias olmadan çalýþmýyor eðer tablo tanýmlarsak.
+;
+
+----
+
+--Write query that returnsthe total list price by each order ids.
+
+SELECT order_id, SUM(list_price) AS total_price
+FROM sale.order_item
+GROUP BY order_id
+;
+
+--with subquery
+
+SELECT so.order_id,
+	   (
+	   SELECT SUM(list_price)
+	   FROM sale.order_item
+	   WHERE order_id = so.order_id
+	   ) AS sum_price
+FROM sale.order_item so
+GROUP BY so.order_id
+
+---
+
+-- Davis Thomas'nýn çalýþtýðý maðazadaki tüm personelleri listeleyin.
+
+SELECT *
+FROM sale.staff
+WHERE store_id = (
+				  SELECT store_id
+				  FROM sale.staff
+				  WHERE first_name = 'Davis' and last_name = 'Thomas'
+				 )
+;
+
+----
+
+-- Charles	Cussona 'ýn yöneticisi olduðu personelleri listeleyin.
+
+SELECT *
+FROM sale.staff
+WHERE first_name = 'Charles' AND last_name = 'Cussona' 
+
+SELECT *
+FROM sale.staff 
+WHERE manager_id = (
+					SELECT staff_id
+					FROM sale.staff
+					WHERE first_name = 'Charles' AND last_name = 'Cussona' 
+					)
+;
+
+----
+
+-- 'Pro-Series 49-Class Full HD Outdoor LED TV (Silver)' isimli üründen pahalý olan ürünleri listeleyin.
+-- Product id, product name, model_year, fiyat, marka adý ve kategori adý alanlarýna ihtiyaç duyulmaktadýr. 
+
+SELECT *
+FROM product.product
+WHERE product_name = 'Pro-Series 49-Class Full HD Outdoor LED TV (Silver)'
+
+
+SELECT product_id, product_name, model_year, list_price
+FROM product.product
+WHERE list_price > (
+					SELECT list_price
+					FROM product.product
+					WHERE product_name = 'Pro-Series 49-Class Full HD Outdoor LED TV (Silver)'
+					)
+;
+
+----
+
+--List all customers who orders on the same date as Laurel Goldammer.
+
+SELECT 
+FROM sale.orders A
+JOIN sale.customer B
+ON A.customer_id = B.customer_id
+WHERE first_name = 'Laurel' AND last_name = 'Goldammer'
+
+SELECT B.first_name, B.last_name, A.order_date
+FROM sale.orders A
+JOIN sale.customer B
+ON A.customer_id = B.customer_id
+WHERE order_date IN
+				   (
+                    SELECT A.order_date
+					FROM sale.orders A
+					JOIN sale.customer B
+					ON A.customer_id = B.customer_id
+					WHERE first_name = 'Laurel' AND last_name = 'Goldammer'
+				   )
+;
+
+----
+
+--List products made in 2021 and their categories other than Game, GPS, or Home Theater.
+
+SELECT A.product_name, A.list_price
+FROM product.product A, product.category B
+WHERE A.category_id = B.category_id
+AND A.model_year = 2021
+AND B.category_name NOT IN ('Game', 'GPS', 'Home Theater')
+
+----
+
+-- Model yýlý 2020 olan ve Receivers Amplifiers kategorisindeki en pahalý üründen daha pahalý ürünleri listeleyin.
+-- Ürün adý, model_yýlý ve fiyat bilgilerini yüksek fiyattan düþük fiyata doðru sýralayýnýz.
+
+SELECT TOP 1 *
+FROM product.product A, product.category B
+WHERE A.category_id = B.category_id 
+	  AND B.category_name = 'Receivers Amplifiers' 
+ORDER BY A.list_price DESC
+
+SELECT product_name, model_year, list_price
+FROM product.product 
+WHERE model_year = 2020
+	  AND list_price > (
+						SELECT TOP 1 A.list_price
+						FROM product.product A, product.category B
+						WHERE A.category_id = B.category_id 
+							  AND B.category_name = 'Receivers Amplifiers' 
+						ORDER BY A.list_price DESC
+					    )
+ORDER BY list_price DESC
+;
+
+--Another Solution
+
+select	product_name, model_year, list_price
+from	product.product
+where	model_year = 2020 and
+		list_price > ALL (    --ALL kullandýðýmýzda birden çok satýr döndüren nested ifadedeki her satýrý kontrol eder.
+			select	B.list_price
+			from	product.category A, product.product B
+			where	A.category_name = 'Receivers Amplifiers' and
+					A.category_id = B.category_id
+			         )
+order by 3 DESC
+;
+
+----
+
+-- Model yýlý 2020 olan ve Receivers Amplifiers kategorisindeki herhangi bir üründen daha pahalý ürünleri listeleyin.
+-- Ürün adý, model_yýlý ve fiyat bilgilerini yüksek fiyattan düþük fiyata doðru sýralayýnýz.
+
+SELECT TOP 1 *
+FROM product.product A, product.category B
+WHERE A.category_id = B.category_id 
+	  AND B.category_name = 'Receivers Amplifiers' 
+ORDER BY A.list_price 
+
+SELECT product_name, model_year, list_price
+FROM product.product 
+WHERE model_year = 2020
+	  AND list_price > (
+						SELECT TOP 1 A.list_price
+						FROM product.product A, product.category B
+						WHERE A.category_id = B.category_id 
+							  AND B.category_name = 'Receivers Amplifiers' 
+						ORDER BY A.list_price 
+					    )
+ORDER BY list_price DESC
+;
+
+-- Another Solution
+
+select	product_name, model_year, list_price
+from	product.product
+where	model_year = 2020 and
+		list_price > ANY (
+			select	B.list_price
+			from	product.category A, product.product B
+			where	A.category_name = 'Receivers Amplifiers' 
+					and	A.category_id = B.category_id
+					)
+order by 3 DESC
+;
+
+
+
+
+
+
+
+
+
+
