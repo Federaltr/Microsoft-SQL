ec28692fa5095aad37a2e90383d9ac3ccca3faf1
Halil Aydın <hllaydnyzn@gmail.com>
2022-06-25 22:28:53 +0300

    some updates

---
+++ SQL-Assignments/Assignment-5/Assignment-5.sql
@@ -0,0 +1,49 @@
+
+--- Assignment-5 ---
+
+-- Create a scalar-valued function that returns the factorial of a number you gave it.
+
+
+CREATE FUNCTION dbo.factorial
+(
+	@input INT
+)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @fact INT = 1
+	WHILE @input > 1
+		BEGIN
+		SET @fact = @input * @fact
+		SET @input = @input - 1
+		END
+	RETURN @fact
+END
+;
+
+SELECT dbo.factorial(4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
